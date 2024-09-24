432a0c30b795b8960faa155b29a481cd55e0202a
Ankit-Karn <ankitkarn10@gmail.com>
2024-09-24 15:38:49 +0530

    bcrypt password comparison

--- task_management_backend/Todo/server.js
+++ task_management_backend/Todo/server.js
@@ -13,125 +13,96 @@ app.use(express.json());
 
 //signup
 app.post('/signup', async (req, res)=>{
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const hashedPassword = await bcrypt.hash(password, 100);
+    try{
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
 
-    await UserModel.create({
-        name: name,
-        email: email,
-        password: hashedPassword
-    })
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.json({
-        message: "You have signed up!"
-    })
+        await UserModel.create({
+            name: name,
+            email: email,
+            password: hashedPassword
+        })
+
+        res.json({
+            message: "You have signed up!"
+        })
+    }catch(e){
+        res.status(404).json({
+            message: "Error while signing up"
+        })
+    }
 })
 
 //signin
-app.post('/signin', async (req, res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
+app.post('/signin', async(req, res)=>{
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
 
-    const response = await UserModel.findOne({
-        email: email
-    })
-    
-    if(response){
-        const token = jwt.sign({
-            id: response._id.toString()
-        }, JWT_SECRET);
+        const user = await UserModel.findOne({
+            email
+        })
+        const passwordMatch = bcrypt.compare(password, user.password);
+        if(user && passwordMatch){
+            const token = jwt.sign({
+                id: user._id.toString()
+            }, JWT_SECRET)
 
         res.json({
-            token
-        })
-
-    }else{
+                token
+            })
+        }
+    }catch(e){
         res.status(403).json({
-            message: "Invalid Credentials"
+            message: "Incorrect credentials"
         })
     }
 })
 
 //auth middleware
-const auth = (req,res,next) =>{
-    const token = req.headers.token;
-    const decodedToken = jwt.verify(token, JWT_SECRET);
-    const username = decodedToken.username;
+const auth = (req, res, next) => {
+    const token = req.headers.authorization;
+    const decodedUserId = jwt.verify(token, JWT_SECRET);
 
-    if(email){
-        req.username = username;
+    if(decodedUserId){
+        req.userId = decodedUserId.id;
         next();
     }else{
-        res.json({
-            message: "You are not logged in"
+        res.status(403).json({
+            message: "Wrong credentials"
         })
     }
 }
 
-//get signed in user Info
-app.get('/user', auth, (req, res)=>{
-    const user = users.find(user => user.username === req.username);
-    if(user){
-        res.json({
-            username : user.username
-        })
-    }else{
-        res.json({
-            message: `cannot find ${username}`
-        })
-    }
-})
-
-// CRUD operation for todo
-
-const todos = [];
-
-// create a task
-app.post('/createTodo', auth, (req, res) => {
+//create todo
+app.post('/todo', auth, async (req,res)=>{
+    const userId = req.userId;
     const title = req.body.title;
-    const newTodo = {
-        id: todos.length+1,
-        username: req.username,
-        title,
-        isCompleted: false
-    };
-    todos.push(newTodo);
-    console.log(todos);
-    res.json(newTodo);
-})
-//Get all task
-app.get('/getTodo', auth, (req, res)=>{
-    const userTodo = todos.filter(todos => todos.username === req.username);
-    res.json(userTodo);
-})
-//delete a task
-app.delete('/deleteTodo/:id', auth, (req, res) =>{
-    const todoIndex = todos.findIndex(todo => todo.username === req.username && todo.id === parseInt(req.params.id));
-    if(todoIndex === -1){
-        res.status(404).json({
-            message: "Todo not found"
-        })
-    }
+    const done = req.body.done;
 
-    todos.splice(todoIndex, 1);
+    await TodoModel.create({
+        userId,
+        title,
+        done
+    })
+
     res.json({
-        message: 'Deleted!'
+        message: `Todo created `
     })
 })
-//Update a task
-app.put('/update/:id', auth, (req, res)=>{
-    const todoId = parseInt(req.params.id);
-    const todo = todos.find(todo => todo.id === todoId && todo.username === req.username);
 
-    if(req.body.title){
-        todo.title = req.body.title;
-    }
-    if(req.body.isCompleted !== undefined){
-        todo.isCompleted = req.body.isCompleted;
-    }
-    res.json(todo);
+//get todos
+app.get('/todos', auth, async (req, res)=> {
+    const userId = req.userId;
+
+    const todo = await TodoModel.find({userId});
+
+    res.json({
+        todo
+    })
 })
 
 app.listen(3001);
